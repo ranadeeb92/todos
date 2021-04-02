b11fa59c567d6c56db98fc0906439925d265031b
Rana Deeb <ranadeeb@gmail.com>
2021-04-01 20:06:58 -0700

    Deployment

--- todos.js
+++ todos.js
@@ -1,3 +1,4 @@
+const config = require('./lib/config');
 const express = require("express");
 const morgan = require("morgan");
 const flash = require("express-flash");
@@ -9,8 +10,8 @@ const catchError = require('./lib/catch-error');
 
 
 const app = express();
-const host = "localhost";
-const port = 3001;
+const host = config.HOST;
+const port = config.PORT;
 const LokiStore = store(session);
 
 app.set("views", "./views");
@@ -29,7 +30,7 @@ app.use(session({
   name: "launch-school-todos-session-id",
   resave: false,
   saveUninitialized: true,
-  secret: "this is not very secure",
+  secret: config.SECRET,
   store: new LokiStore({}),
 }));
 
